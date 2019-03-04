7844bd15b59b3aec4dae14697e1678eb81f0b34a
weiyu <weiyu005@ke.com>
2019-03-04 15:16:15 +0800

    修改远程调用demo

--- src/main/scala/willem/weiyu/scala/akka/TeacherActor.scala
+++ src/main/scala/willem/weiyu/scala/akka/TeacherActor.scala
@@ -6,34 +6,34 @@ import com.typesafe.config.ConfigFactory
 class TeacherActor extends Actor{
 
   override def receive: Receive = {
-    case "1+1等于多少?"           => {
-      sender ! "1+1等于2"
+    case "1+1等于多少?" => {
+      sender ! "1+1=2"
     }
-    case "历史上规模最大的众筹行动是什么？" => {
-      sender ! "历史上规模最大的众筹行动是 +1s"
+    case "中国历史上最后一位皇帝是谁?" => {
+      sender ! "中国历史上最后一位皇帝是爱新觉罗·溥仪"
     }
-    case "腾讯第一网红是谁？"     => {
-      sender ! "腾讯第一网红是\"我去\""
+    case "腾讯第一网红是谁?" => {
+      sender ! "腾讯第一网红是企鹅"
     }
-    case _              => {
+    case _ => {
       sender ! "这个问题，老师也得查查书"
     }
   }
 }
 
-object TeacherService {
+object TeacherSimulator {
 
   def main(args: Array[String]): Unit = {
+    //读入配置
     val config = ConfigFactory
       .parseResources("system.conf")
       .getConfig("RemoteServerSideActor")
-    //读入配置
-    val system = ActorSystem("teacherService",  config)
+
     //使用配置，建立 Actor 模型系统 ActorSystem。
     //ActorSystem 是访问 Actor 模型系统的接口，类似于 Spark 的 SparkContext。
+    val system = ActorSystem("teacherService",  config)
 
+    //创建teacherActor，返回一个引用，teacherActor 是 Actor 的名，客户端需要用
     system.actorOf(Props[TeacherActor], "teacherActor")
-    //创建TearcherActor，返回一个引用
-    //teacherActor 是 Actor 的名，客户端需要用
   }
 }
